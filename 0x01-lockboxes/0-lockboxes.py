775a47095d10fdca254177ba155d042ecc05ccec
Sibongile <sibongile.nhlema@gmail.com>
2024-06-06 18:49:31 +0200

    created the general layout of the lockboxes method

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+This module handles the implementation of lockboxes.
+It includes a function that checks whether all boxes
+can be unlocked.
+'''
+
+def canUnlockAll(boxes):
+    '''
+    Method that checks whether all boxes can be unlocked
+    Args:
+        boxes(list of lists): boxes given
+    '''
+    pass
