a5a8cf863ffa560892e0951a9c75da6086bda963
Sibongile <sibongile.nhlema@gmail.com>
2024-06-20 22:14:23 +0200

    updated the script to also use finally

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-'''
-script that reads stdin line by line and computes metrics
-Input format: <IP Address> - [<date>]
-"GET /projects/260 HTTP/1.1" <status code> <file size>
-
+"""
+Script that reads stdin line by line and computes metrics
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
 (if the format is not this one, the line must be skipped)
-
 After every 10 lines and/or a keyboard interruption (CTRL + C), print these statistics from the beginning:
 Total file size: File size: <total size>
 where <total size> is the sum of all previous <file size> (see input format above)
@@ -14,7 +11,8 @@ possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
 if a status code doesn’t appear or is not an integer, don’t print anything for this status code
 format: <status code>: <number>
 status codes should be printed in ascending order
-'''
+"""
+
 import sys
 import re
 
@@ -24,17 +22,24 @@ log_line_regex = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] "GET /proje
 total_size = 0
 status_code_counts = {}
 
+def print_statistics():
+    print(f"File size: {total_size}")
+    for code in sorted(status_code_counts.keys()):
+        print(f"{code}: {status_code_counts[code]}")
+    print()
+
 try:
     line_count = 0
     for line in sys.stdin:
-        line_count += 1
         line = line.strip()
         
-        # Match the line against the regex expression
+        # Match the line against the regex
         match = re.match(log_line_regex, line)
         if match:
+            line_count += 1
             status_code = int(match.group(3))
             file_size = int(match.group(4))
+            
             # Update total file size
             total_size += file_size
             
@@ -44,16 +49,13 @@ try:
             else:
                 status_code_counts[status_code] = 1
             
-            # Print statistics for every 10 lines
+            # Print statistics every 10 lines
             if line_count % 10 == 0:
-                print(f"File size: {total_size}")
-                for code in sorted(status_code_counts.keys()):
-                    print(f"{code}: {status_code_counts[code]}")
-                print()
+                print_statistics()
 
 except KeyboardInterrupt:
-    # Print final statistics on keyboard interruptions
-    print(f"File size: {total_size}")
-    for code in sorted(status_code_counts.keys()):
-        print(f"{code}: {status_code_counts[code]}")
-    sys.exit(0)
+    pass
+
+finally:
+    # Print final statistics on keyboard interrupt
+    print_statistics()
