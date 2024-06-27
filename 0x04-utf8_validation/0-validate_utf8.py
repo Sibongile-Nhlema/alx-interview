93a6dcded427251fa8f5b2ec17461393d50dbda5
Sibongile <sibongile.nhlema@gmail.com>
2024-06-27 22:35:25 +0200

    implemented the method again but without the python bitwise operators

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,50 +6,40 @@ Module for the implementation of the validUTF6 method
 
 def validUTF8(data):
     '''
-    method that determines if a given data set
-    represents a valid UTF-8 encoding
+    Method that determines if a given data set
+    represents a valid UTF-8 encoding.
     Args:
-        data (set): given data set
+        data (list): Given data set represented by a list of integers.
     Returns:
-        Boolean: true is is valid, otherwise false
-
-    Prep:
-    - (1 << 7): Shifts 1 to the left by 7 positions, resulting in 0b10000000.
-    - (1 << 6 | 1 << 5): Shifts 1 to the left by 6 positions,
-    OR shifts 1 to the left by 5 positions,
-    resulting in 0b11100000.
-    - (1 << 5): Shifts 1 to the left by 5 positions, resulting in 0b11000000.
-    - (1 << 4): Shifts 1 to the left by 4 positions, resulting in 0b10000.
+        bool: True if data is a valid UTF-8 encoding, otherwise False.
     '''
     start_mask = [0b10000000, 0b11100000, 0b11110000, 0b11111000]
+    continuation_mask = 0b11000000
     # Number of bytes each UTF-8 character can occupy
     num_of_bytes = [1, 2, 3, 4]
 
     # Iterate through the given data set
     i = 0
     while i < len(data):
-        # Find the num_of_bytes for current character
+        # Find the number of bytes for current character
 
-        # Check first byte to find the num_of_bytes in the sequence
+        # Check the first byte to determine the number of bytes in the sequence
 
-        # equals 0b10000000
-        if data[i] >= (1 << 7):
+        if data[i] >= 0b11111000:
+            # 5-byte and larger sequences - invalid in UTF-8
             return False
-
-        # equals 0b11100000
-        elif data[i] >= (1 << 6 | 1 << 5):
+        elif data[i] >= 0b11110000:
+            # 4-byte sequence - valid
+            nbytes = 4
+        elif data[i] >= 0b11100000:
+            # 3-byte sequence - valid
             nbytes = 3
-
-        # equals 0b11000000
-        elif data[i] >= (1 << 5):
+        elif data[i] >= 0b11000000:
+            # 2-byte sequence - valid
             nbytes = 2
-
-        # equals 0b10000000
-        elif data[i] >= (1 << 4):
-            nbytes = 1
-
         else:
-            return False
+            # 1-byte sequence - valid
+            nbytes = 1
 
         # Check if there are enough bytes left in data
         if i + nbytes > len(data):
@@ -57,7 +47,7 @@ def validUTF8(data):
 
         # Validate the sequence
         for j in range(1, nbytes):
-            if (data[i + j] & (1 << 7) | (1 << 6)) != (1 << 7):
+            if (data[i + j] & 0b11000000) != 0b10000000:
                 return False
 
         # Move to the next character
