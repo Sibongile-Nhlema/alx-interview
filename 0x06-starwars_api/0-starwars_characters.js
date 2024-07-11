af9f2fdc41953ebcda26d3d88c770cf071ae393c
Sibongile <sibongile.nhlema@gmail.com>
2024-07-11 21:06:41 +0200

    created a function that returns a promise in order to maintain the right order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,8 +4,21 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-// Make an HTTP GET request to the Star Wars API for movie id given
-request(url, function (error, response, body) {
+// Function to make a request and return a promise
+function fetchCharacter(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+}
+
+// Make an HTTP GET request to the Star Wars API for the specified movie
+request(url, async function (error, response, body) {
   if (error) {
     console.error(error);
     return;
@@ -17,18 +30,12 @@ request(url, function (error, response, body) {
   // Get the list of characters
   const characters = film.characters;
 
-  characters.forEach((characterUrl) => {
-    // Make an HTTP GET request to get the details for each character
-    request(characterUrl, function (charError, charResponse, charBody) {
-      if (charError) {
-        console.error(charError);
-        return;
-      }
-
-      // Parse the JSON response body to get the character data
-      const character = JSON.parse(charBody);
-
-      console.log(character.name);
-    });
-  });
+  // Fetch all character names while maintaining order
+  try {
+    const characterNames = await Promise.all(characters.map(fetchCharacter));
+    // Print each character name in the order they come through
+    characterNames.forEach(name => console.log(name));
+  } catch (err) {
+    console.error(err);
+  }
 });
