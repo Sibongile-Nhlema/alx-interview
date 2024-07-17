1508ec8a0ce80d7ae1e20ac7d2b1abf7da43f2cc
Sibongile <sibongile.nhlema@gmail.com>
2024-07-17 21:30:46 +0200

    removed the type annotation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,10 +2,9 @@
 '''
 Rotate 2D Matrix interview project
 '''
-from typing import List
 
 
-def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+def rotate_2d_matrix(matrix):
     '''
     Rotates an n x n 2D matrix 90 degrees clockwise in-place
     Args:
