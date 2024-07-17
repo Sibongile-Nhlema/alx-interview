b1bdadedaafbb1064593409036a87407a1787903
Sibongile <sibongile.nhlema@gmail.com>
2024-07-17 21:28:16 +0200

    completed the interview question

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+Rotate 2D Matrix interview project
+'''
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+    '''
+    Rotates an n x n 2D matrix 90 degrees clockwise in-place
+    Args:
+        param matrix: List of lists of integers representing the 2D matrix
+    '''
+    number = len(matrix)
+
+    for i in range(number):
+        for j in range(i, number):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+    for i in range(number):
+        matrix[i].reverse()
