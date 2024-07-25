140b64f8dc4d765b1bfb34cc635856fcf88678ba
Sibongile <sibongile.nhlema@gmail.com>
2024-07-25 20:17:33 +0200

    fixed the pycodestyle errors

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,6 +3,7 @@
 Module for the making change problem
 '''
 
+
 def makeChange(coins, total):
     '''
     Determines the fewest number of coins needed
@@ -16,7 +17,7 @@ def makeChange(coins, total):
     '''
     if total <= 0:
         return 0
-    
+
     # Initialize the min_coins list with infinity value (highest)
     min_coins = [float('inf')] * (total + 1)
     min_coins[0] = 0
@@ -25,7 +26,7 @@ def makeChange(coins, total):
         for amount in range(coin, total + 1):
             min_coins[amount] = min(min_coins[amount],
                                     min_coins[amount - coin] + 1)
-    
+
     result = min_coins[total] if min_coins[total] != float('inf') else -1
 
     return result
