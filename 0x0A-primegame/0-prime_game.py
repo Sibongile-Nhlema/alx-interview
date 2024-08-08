be0e44d319523fa150a02b0319f917b9f3cd975d
Sibongile <sibongile.nhlema@gmail.com>
2024-08-08 21:09:25 +0200

    rewrote the interview question

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 '''
-This module helps find out who wins a game
+Module that helps determine the winner of a game
 where you remove prime numbers and their multiples
 from a list of numbers.
 '''
 
-
 def sieve_of_eratosthenes(max_n):
     '''
-    Finds all prime numbers up to max_n
-    using a method called Sieve of Eratosthenes
+    Generates prime numbers up to max_n
+    using the Sieve of Eratosthenes.
     '''
     sieve = [True] * (max_n + 1)
     sieve[0], sieve[1] = False, False  # 0 and 1 are not primes
@@ -20,42 +19,37 @@ def sieve_of_eratosthenes(max_n):
                 sieve[j] = False
 
     primes = [i for i in range(2, max_n + 1) if sieve[i]]
-
-    return primes, sieve
-
-
-def count_prime_multiples(sieve, n):
-    '''
-    Counts how many primes and their multiples
-    can be removed from 1 to n
-    '''
-    count = 0
-    for i in range(2, n + 1):
-        if sieve[i]:
-            count += 1
-            for j in range(i, n + 1, i):
-                sieve[j] = False  # Mark multiples as not prime
-
-    return count
+    return sieve
 
 
 def isWinner(x, nums):
     '''
-    Finds out who the winner is
+    Determines the winner of the game.
     '''
     if not nums or x < 1:
         return None
 
     max_n = max(nums)
-    primes, sieve = sieve_of_eratosthenes(max_n)
+    sieve = sieve_of_eratosthenes(max_n)
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
+        if n == 1:
+            ben_wins += 1
+            continue
+        
+        count = 0
         sieve_copy = sieve[:n + 1]
-        prime_count = count_prime_multiples(sieve_copy, n)
-        if prime_count % 2 == 1:
+        
+        for i in range(2, n + 1):
+            if sieve_copy[i]:
+                count += 1
+                for j in range(i, n + 1, i):
+                    sieve_copy[j] = False
+        
+        if count % 2 == 1:
             maria_wins += 1
         else:
             ben_wins += 1
@@ -66,3 +60,4 @@ def isWinner(x, nums):
         return "Ben"
     else:
         return None
+
